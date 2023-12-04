a1ccf4d8b049ab697b6193d37249269556cb2521
cstepin <87153144+cstepin@users.noreply.github.com>
2023-12-04 17:06:10 -0500

    Create unique_variable_test.lisp

---
+++ benchmarks/unique_variable_test.lisp
@@ -0,0 +1,6 @@
+(define (f x y) (+ y (+ y (+ x y))))
+(define (g y x) (+ x (- 4 (- y y))))
+(define (h xy x y) (- y (+ xy (+ x (- xy (+ y xy))))))
+(define (i x y) (- (+ x y) (- (- x (+ y x)) (+ x y))))
+(define (j xx y) (- y (+ xx (+ xx (- xx (+ y xx))))))
+
