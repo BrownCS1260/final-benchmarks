3fc38f76843a9f3115dc6801589573b9095bdb90
Joe Dodson <74784507+jcvdodson@users.noreply.github.com>
2023-12-09 15:33:36 -0500

    fix syntax

--- benchmarks/simple-inline.lisp
+++ benchmarks/simple-inline.lisp
@@ -4,8 +4,8 @@
 (define (decrement x) (- x 1))
 
 (define (compute x)
-  (let ((result1 (add x (increment x)))
-        (result2 (subtract x (decrement x))))
-    (+ result1 result2)))
+  (let ((result1 (add x (increment x))))
+    (let (result2 (subtract x (decrement x)))
+    (+ result1 result2))))
 
 (print (compute 4))
