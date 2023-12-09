3d4d9a10a5ed9cc7b0891bab8ac7bcba537f2484
Joe Dodson <74784507+jcvdodson@users.noreply.github.com>
2023-12-09 15:19:11 -0500

    Delete benchmarks/inline-constant-prop-conditional.lisp

--- benchmarks/inline-constant-prop-conditional.lisp
+++
@@ -1,9 +0,0 @@
-(define (check x) (if (= x 0) 100 200))
-(define (addAndSubtract x y) (+ x (- y 3)))
-
-(define (process x)
-  (let ((result1 (check x))
-        (result2 (addAndSubtract x 5)))
-    (+ result1 result2)))
-
-(print (process 0))
