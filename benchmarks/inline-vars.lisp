ce4461eddc254f60663dc35eaa925cb43343bc75
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 17:58:57 -0500

    work

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -1,4 +1,7 @@
 (define (sum_2 i j) (+ i j))
 (define (sum_3 j k) (+ i (+ j k)))
 
-(let ((i 2)) (let ((j 3)) (let ((k 4)) (print (sum_2 (sum3 i j k) (sum_3 i j k))))))
+(let ((i 2))
+    (let ((j 3))
+        (let ((k 4))
+        (print (sum_2 (sum3 i j k) (sum_3 i j k))))))
