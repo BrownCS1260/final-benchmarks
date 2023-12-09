05a31722532d47f1cdc207d78fda8bc922ef27b7
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 17:26:22 -0500

    remove newlines

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -1,12 +1,3 @@
 (define (f_2 i j) (+ i j))
 (define (f i j k) (+ i (+ j k)))
-(
-  let ((i 2))
-  (
-    let ((j 3))
-    (
-        let ((k 4))
-        (print (f_2 (f i j k) (f i j k)))
-    )
-  )
-)
+(let ((i 2)) (let ((j 3)) (let ((k 4)) (print (f_2 (f i j k) (f i j k))))))
