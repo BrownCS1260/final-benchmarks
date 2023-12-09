42819d544cf400acf19323567f53cc0d3a726ad5
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 16:42:21 -0500

    fix test

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -2,9 +2,9 @@
 (define (f f_1 i j_1) (if (< i f_1) f_1 f_1))
 (define (f_2 i f k_1) (if (= f i) k_1 f))
 
-(let ((i (f_1 1 2 3))) i = 3
+(let ((i (f_1 1 2 3)))
   (
-    let ((k_1 (f 4 5 6))) k_1 = 4
+    let ((k_1 (f 4 5 6)))
     (
         (print (f_2 (f_1 i 2 k_1) 2 3))
     )
