d0468af8156a7a85e8847114aacd9cd1044040d6
Joe Dodson <74784507+jcvdodson@users.noreply.github.com>
2023-12-09 14:49:13 -0500

    wrong place

--- constant-prop-conditional.lisp
+++
@@ -1,9 +0,0 @@
-(define (check x) (if (= x 0) 100 200))
-(define (multiply x y) (* x y))
-
-(define (process x)
-  (let ((result1 (check x))
-        (result2 (multiply x 5)))
-    (+ result1 result2)))
-
-(print (process 0))
