5f9ba06ccdd10a4a9c2f5772ce4eadaeda9d6c90
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 16:59:34 -0500

    fix

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -1,12 +1,13 @@
-(define (f_1 i_1 j_1 k_1) (if (= i_1 j_1) j_1 k_1))
-(define (f f_1 i j_1) (if (< i f_1) f_1 f_1))
-(define (f_2 i f k_1) (if (= f i) k_1 f))
+(define (f i j k) (+ i (+ j k)))
+(define (f_1 i j) (- i j))
+(define (f_2 j k) (+ j k))
 
-(let ((i (f_1 1 2 3)))
+(let ((i 3))
   (
-    let ((k_1 (f 4 5 6)))
+    let ((j 4))
     (
-        (print (f_2 (f_1 i 2 k_1) 2 3))
+        let ((k 5))
+        (print (f (f k (f_1 i j) (f_2 j k) j k)))
     )
   )
 )
