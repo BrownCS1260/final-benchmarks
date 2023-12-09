0ea33902546cfa9686ee43cf2da9a540f87b83ad
Joe Dodson <74784507+jcvdodson@users.noreply.github.com>
2023-12-09 15:36:09 -0500

    fix syntax

--- benchmarks/constant-prop.lisp
+++ benchmarks/constant-prop.lisp
@@ -1,8 +1,8 @@
 (define (constant-function)
-  (let ((a 10)
-        (b 20)
-        (c 30))
-    (+ a b c)))
+  (let ((a 10))
+    (let ((b 20))    
+       (let ((c 30))
+    (+ a b c)))))
 
 (define (compute x)
   (if (= x 0)
