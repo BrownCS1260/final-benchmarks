67c269197ab444b30f641f327f582c38f9fcca3b
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 17:30:58 -0500

    another fix

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -1,3 +1,4 @@
-(define (f_2 i j) (+ i j))
-(define (f i j k) (+ i (+ j k)))
-(let ((i 2)) (let ((j 3)) (let ((k 4)) (print (f_2 (f i j k) (f i j k))))))
+(define (sum i j) (+ i j))
+(define (sum_3 j k) (+ i (+ j k)))
+
+(let ((i 2)) (let ((j 3)) (let ((k 4)) (print (sum (sum3 i j k) (sum3 i j k))))))
