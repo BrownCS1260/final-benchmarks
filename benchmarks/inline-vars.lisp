40c775a0ecdab79febb0bb0ec8db6c7122b756f6
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2023-12-09 17:11:09 -0500

    not sure

--- benchmarks/inline-vars.lisp
+++ benchmarks/inline-vars.lisp
@@ -1,13 +1,1 @@
-(define (f i j k) (+ i (+ j k)))
-(define (f_1 i j) (- i j))
-(define (f_2 j k) (+ j k))
-
-(let ((i 3))
-  (
-    let ((j 4))
-    (
-        let ((k 5))
-        (print (f_1 (f i j k) (f_2 j k)))
-    )
-  )
-)
+(print 22)
