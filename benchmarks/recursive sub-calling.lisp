3af30c3b15b1cced59c2eaba56f2f53631dd6e1d
jsherbst <98423061+jsherbst@users.noreply.github.com>
2023-12-10 04:33:23 +0000

    jherbst1

--- benchmarks/recursive sub-calling.lisp
+++ benchmarks/recursive sub-calling.lisp
@@ -1,2 +1,2 @@
 (define (decreaser n) (if (= n 0) n (if (= n 1) n (if (= n n) (decreaser (if true (sub1 n) 4)) 5))))
-(print (+ (decreaser 10) (+ (decreaser 10) (decreaser 10))))
+(print (decreaser 10))
