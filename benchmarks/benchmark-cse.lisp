64cc5ea1318a30ae2e66cb34544d551aae9585ed
liuser23 <77646042+liuser23@users.noreply.github.com>
2023-12-09 20:50:14 -0500

    change benchmark-lisp

--- benchmarks/benchmark-cse.lisp
+++ benchmarks/benchmark-cse.lisp
@@ -1,5 +1,5 @@
 (define (sum-to x)
   (if (= x 0) 0
     (+ x (sum-to (sub1 x)))))
-(let ((x (read-num)) (y (read-num)))
-  (print (+ (+ (+ (- (sum-to x) (+ (sum-to x) (sum-to y))) (+ (sum-to x) (+ (sum-to x) (sum-to (add1 y))))) (sum-to y)) (sum-to x))))
+(let ((x (read-num)))
+  (print (+ (+ (+ (- (sum-to x) (+ (sum-to x) (sum-to x))) (+ (sum-to x) (+ (sum-to x) (sum-to (add1 x))))) (sum-to x)) (sum-to x))))
