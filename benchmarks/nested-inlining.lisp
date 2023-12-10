5b29bcb37ae3f8b2b33a47c35dbbfdecd3226293
SolarPowerWu <solarpowerwu@gmail.com>
2023-12-09 19:45:04 -0500

    jwu191

--- benchmarks/nested-inlining.lisp
+++ benchmarks/nested-inlining.lisp
@@ -1,17 +1,3 @@
 (define (a x y) (+ x y))
 
-(define (b x) (a x 2))
-
-(define (c x) (a x 3))
-
-(define (d x) (a x 4))
-
-(define (e x) (a x 5))
-
-(define (f x) (a x 6))
-
-(define (g x) (f (e (d x))))
-
-(define (h x) (c (b x)))
-
-(print (g (h (read-num))))
+(print (a (read-num) 2))
